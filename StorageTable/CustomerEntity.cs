90f8d7bef4f91868257c66293ac39b5baac5ee33
Mohammad Nadeem <nadeemlari@hotmail.com>
2022-09-30 22:32:56 +0530

    knn

---
+++ StorageTable/CustomerEntity.cs
@@ -0,0 +1,16 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace StorageTable;
+
+public class CustomerEntity : ITableEntity
+{
+    public string PartitionKey { get; set; } // tenant
+    public string RowKey { get; set; } // customer id
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+    public string Email { get; set; }
+    public string ApimId { get; set; }
+   public string Tenant { get; set; }
+    public string BaseUrl { get; set; }
+}
--- StorageTable/Program.cs
+++ StorageTable/Program.cs
@@ -1,37 +1,44 @@
-﻿using Azure.Identity;
+﻿using Azure;
+using Azure.Identity;
 using Azure.Data.Tables;
-using Azure;
-using System.Collections.Concurrent;
+using StorageTable;
 
-TableClient tableClient = new TableClient(new Uri("https://mnlaristorage.table.core.windows.net"),
-                                                "Customers",
-                                                new DefaultAzureCredential());
+var tableClient = new TableClient(new Uri("https://mnlaristrorage.table.core.windows.net"),
+    "Customers", new DefaultAzureCredential());
+const string partitionKey = "APIM";
 try
 {
     // Create the table.
-   // await tableClient.CreateIfNotExistsAsync();
-    //var t = new CustomerEntity { RowKey = "mnlari@hotmail.com" , PartitionKey="par1" ,PhoneNumber="9971322993"};
+    // await tableClient.CreateIfNotExistsAsync();
+    // for (var i = 1; i < 100; i++)
+    // {
+    //     var entity = new CustomerEntity
+    //     {
+    //         RowKey = $"customerId{i}",
+    //         PartitionKey = partitionKey,
+    //         Email = $"mnlari{i}@hotmail.com",
+    //         Tenant = "cloud",
+    //         BaseUrl = $"https://cloud.iot.solenis.com",
+    //         ApimId = $"apimId{i}"
+    //     };
+    //     await tableClient.AddEntityAsync(entity);
+    // }
 
-    //var r = await tableClient.AddEntityAsync<CustomerEntity>(t);
 
-    Pageable<CustomerEntity> queryResultsFilter = tableClient.Query<CustomerEntity>(filter: $"PhoneNumber eq '9971322993'");
-    foreach (var item in queryResultsFilter)
+    // var queryResultsFilter =
+    //     tableClient.Query<CustomerEntity>(filter: $"PhoneNumber eq '9971322993'");
+    // foreach (var item in queryResultsFilter)
+    // {
+    // }
+    var x = 0;
+    while (x< 100)
     {
-
+        x++;
+        var y = tableClient.Query<CustomerEntity>(x => x.PartitionKey == partitionKey && x.RowKey == "customerId1", 1).FirstOrDefault();
+        Console.WriteLine($"{x}");
     }
-
 }
 catch (Exception e)
 {
     Console.WriteLine("Exception: {0}", e.Message);
-}
-
-public class CustomerEntity : ITableEntity
-{
-    public string PartitionKey { get; set; }
-    public string RowKey { get; set; }
-    //public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public DateTimeOffset? Timestamp { get; set; }
-    public ETag ETag { get; set; }
 }
